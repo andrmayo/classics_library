cf4961ff38ab8e16f870299cce1e036d82165ecb
Andrew Mayo <acmayo399@gmail.com>
2025-06-17 18:32:27 -0400

    working on csv2marc testing

--- testing/test_mrc2csv2mrc.py
+++ testing/test_mrc2csv2mrc.py
@@ -3,11 +3,13 @@ from pymarc import MARCReader
 from csv_converter import to_csv, to_marc
 
 # using PGA-Australiana.mrc test ability to convert to csv, and backconvert to the exact same marc file.
-src_path = Path("./PGA-Australiana.mrc")
-dest = src_path.parent / f"{src_path.name.split('.')[0]}.csv"
+src_path = Path("./testing/PGA-Australiana.mrc")
+dest = src_path.parent / f"{src_path.stem}.csv"
+to_csv(src_path, dest)
+newmarc_dest = dest.parent / f"{dest.stem}_fromMARC.mrc"
+to_marc(dest, newmarc_dest)
 
 def test_2csv():
-    to_csv(src_path, dest)
     # check that csv file has the right numer of lines
     record_count = 0
     with open(src_path, "rb") as f:
@@ -20,5 +22,14 @@ def test_2csv():
             csv_count += 1
     # note that csv file should have one more line than marc file has records, because of header line
     assert csv_count == record_count + 1, f"csv_count is {csv_count} and record_count is {record_count}"
+
+def test_2marc():
+    with open(src_path, "rb") as f1, open(newmarc_dest, "rb") as f2:
+        reader1 = MARCReader(f1)
+        reader2 = MARCReader(f2)
+        for line1, line2 in zip(reader1, reader2):
+            print(line1, line2)
+
+test_2marc()
 
     
